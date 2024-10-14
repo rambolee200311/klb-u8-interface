2e1bb0b65a2db93f6dc5e4179dfc47697ca166d6
rambolee200311 <rambolee200311@hotmail.com>
2024-10-14 22:58:44 +0800

    20241014 update

--- BidlinkU8Interface/Controllers/PurchaseOrderController.cs
+++ BidlinkU8Interface/Controllers/PurchaseOrderController.cs
@@ -13,9 +13,9 @@ namespace BidlinkU8Interface.Controllers
 {
     public class PurchaseOrderController : ApiController
     {
-        [Route("purchase/add")]
+        [Route("purchaseorder/add")]
         [HttpPost]
-        public KlbU8Vouch.GLVouch.Result addDispatchList([FromBody]InMain inMain)
+        public KlbU8Vouch.GLVouch.Result addPurchaseOrder([FromBody]InMain inMain)
         {
             Log.AddInfo("PurchaseOrderController.addDispatchList()", "Begin");
             KlbU8Vouch.GLVouch.Result result = new KlbU8Vouch.GLVouch.Result();
@@ -34,5 +34,35 @@ namespace BidlinkU8Interface.Controllers
             Log.AddInfo("PurchaseOrderController.addDispatchList()", "End");
             return result;
         }
+        [Route("purchaseorder/getsample")]
+        [HttpGet]
+        public InMain getSample()
+        {
+            InMain inMain = new InMain();
+            inMain.ds_sequence=3;
+            inMain.oa_mid= "106114";
+            inMain.vencode= "01001";
+            inMain.depcode= "01";
+            inMain.personcode= "01001";
+            inMain.cpocode= "KLB20221055-120-GZ001";
+            inMain.currency= "人民币";
+            inMain.pucode= "11";
+            inMain.memo= "测试测试测试";
+            inMain.ddate= "2024-10-11";
+            inMain.cmaker= "宋媛媛";
+            inMain.itemclass= "97";
+            inMain.itemcode= "A001";
+            inMain.details = new List<InDetail>();
+            InDetail inDetail = new InDetail();
+            inDetail.oa_did = "3";
+            inDetail.invcode= "I72030134";
+            inDetail.qty= 4;
+            inDetail.txtrate= 13;
+            inDetail.unitprice= 1769.91m;
+            inDetail.taxprice = 2000;
+            inMain.details.Add(inDetail);
+            return inMain;
+        }
+
     }
 }
